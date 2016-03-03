a5e950a387036195ebe271b2a2eada45cb76d231
kexin chen <kexinchen7@gmail.com>
2016-03-03 00:36:34 -0600

    binary tree - part 1

--- 102.BinaryTreeLevelOrderTraversal.java
+++ 102.BinaryTreeLevelOrderTraversal.java
@@ -5,12 +5,209 @@ BFS: level searching
 */
 
 // DFS depth-limted DFS
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+ 		List<List<Integer>> results = new ArrayList<List<Integer>>();
 
+ 		if(root == null) {
+ 			return results;
+ 		}
+
+ 		int maxLevel = 0;
+ 		while(true) {
+ 			List<Integer> level = new ArrayList<Integer>();
+ 			dfs(root, level, 0, maxLevel);
+ 			if(level.size() == 0) {
+ 				break;
+ 			}
+ 			results.add(level);
+ 			maxLevel++;
+ 		}
+
+ 		return results;
+    }
+
+    private void dfs(TreeNode root, List<Integer> level, int curLevel, int maxLevel) {
+    	if(root == null || curLevel > maxLevel) {
+    		return;
+    	}
+
+    	if(curLevel == maxLevel) {
+    		level.add(root.val);
+    		return;
+    	}
+
+    	dfs(root.left, level, curLevel+1, maxLevel);
+    	dfs(root.right, level, curLevel+1, maxLevel);
+    }
+
+} 
 
 
 // BFS
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+		if(root == null) {
+			return result;
+		}
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+
+		while(!queue.isEmpty()) {
+			List<Integer> curRes = new ArrayList<Integer>();
+			int size = queue.size();
+			for(int i = 0; i < size; ++i) {
+				TreeNode temp = queue.poll();
+				curRes.add(temp.val);
+				
+				if(temp.left != null) {
+					queue.offer(temp.left);
+				}
+
+				if(temp.right != null) {
+					queue.offer(temp.right);
+				}
+			}
+
+			result.add(curRes);
+		}
+
+		return result;
+    }
+
+} 
 
 
-// BFS with two queues
+
+
+
+// BFS with two queues - 1
+public class Solution {
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> results = new ArrayList<List<Integer>>();
+		if(root == null) {
+			return results;
+		}
+
+        List<TreeNode> Q1 = new ArrayList<TreeNode>();
+        List<TreeNode> Q2 = new ArrayList<TreeNode>();
+
+        Q1.add(root);
+		List<Integer> level = new ArrayList<Integer>();
+
+		while(Q1.size() != 0) {
+		    
+			TreeNode node = Q1.remove(0);
+			level.add(node.val);
+
+			if(node.left != null) {
+				Q2.add(node.left);
+			}
+
+			if(node.right != null) {
+				Q2.add(node.right);
+			}
+            
+			if(Q1.size() == 0) {
+				List<TreeNode> temp;
+				temp = Q2;
+				Q2 = Q1;
+				Q1 = temp;
+
+				results.add(level);
+				level = new ArrayList<Integer>();
+			}
+
+		}
+
+		return results;
+	}
+
+}
+
+
+// BFS two queues - 2
+public class Solution {
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> results = new ArrayList<List<Integer>>();
+		if(root == null) {
+			return results;
+		}
+
+        List<TreeNode> Q1 = new ArrayList<TreeNode>();
+        List<TreeNode> Q2 = new ArrayList<TreeNode>();
+
+        Q1.add(root);
+		
+		while(Q1.size() != 0) {
+		    List<Integer> level = new ArrayList<Integer>();
+		    int size = Q1.size();
+			for(int i = 0; i < size; i++) {
+			    TreeNode node = Q1.remove(0);
+			    level.add(node.val);
+			    if(node.left != null) {
+			        Q2.add(node.left);
+			    }
+			    
+			    if(node.right != null) {
+			        Q2.add(node.right);
+			    }
+			}
+			
+			results.add(level);
+			List<TreeNode> temp;
+			temp = Q2;
+			Q2 = Q1;
+			Q1 = temp;
+		}
+
+		return results;
+	}
+
+}
+
+
 
 // BFS with dummy node
+public class Solution {
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> results = new ArrayList<List<Integer>>();
+		if(root == null) {
+			return results;
+		}
+
+        Queue<TreeNode> Q = new LinkedList<TreeNode>();
+        Q.offer(root);
+        Q.offer(null);
+
+        List<Integer> level = new ArrayList<Integer>();
+		while(!Q.isEmpty()) {
+			TreeNode node = Q.poll();
+			if(node == null) {
+				results.add(level);
+				if(Q.isEmpty()) {
+					break;
+				}
+				level = new ArrayList<Integer>();
+				Q.offer(null);
+				continue;
+			}
+
+			level.add(node.val);
+
+			if(node.left != null) {
+				Q.offer(node.left);
+			}
+
+			if(node.right != null) {
+				Q.offer(node.right);
+			}
+		}
+
+		return results;
+	}
+
+}
--- 98.ValidateBinarySearchTree.java
+++ 98.ValidateBinarySearchTree.java
@@ -1,9 +1,97 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 
+// by the property of in-order traversal
 
-// traverse, in-order traversal
+// firstNode is used to skip the "if(!firstNode && lastVal >= root.val){}" when the first enter
+public class Solution {
+    
+    private boolean firstNode = true;
+    private int lastVal;
+    
+    public boolean isValidBST(TreeNode root) {
+        
+        if(root == null) {
+            return true;
+        }
+        
+        if(!isValidBST(root.left)) {
+            return false;
+        }
+        
+        if(!firstNode && lastVal >= root.val) {
+            return false;
+        }
+        
+        firstNode = false;
+        lastVal = root.val;
+        
+        if(!isValidBST(root.right)) {
+            return false;
+        }
+        
+        return true;
+        
+    }
+    
+    
+}
 
 
 // divide and conquer
+class ReturnType {
+    boolean isBst;
+    int maxValue;
+    int minValue;
 
-//
+    ReturnType(boolean is_Bst, int max_Value, int min_Value) {
+        isBst = is_Bst;
+        maxValue = max_Value;
+        minValue = min_Value;
+    }
+}
+
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        ReturnType r = helper(root);
+        return r.isBst;
+    }
+
+    public ReturnType helper(TreeNode root) {
+        if(root == null) {
+            return new ReturnType(true, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+
+        // divide
+        ReturnType left = helper(root.left);
+        ReturnType right = helper(root.right);
+
+        // conquer
+        // any side return false
+        if(!left.isBst || !right.isBst) {
+            return new ReturnType(false, 0, 0);
+        }
+
+        // both sides return true, 
+        // but when add root, one of sides doesn't satisfy
+        if(root.left != null && root.val <= left.maxValue || 
+           root.right != null && root.val >= right.minValue) {
+            return new ReturnType(false, 0 , 0);
+        }
+
+        // both sides return true,
+        // and when add root, both satisfy
+        // be careful when the root doesn't have two childs, i.e. one child or no child
+        return new ReturnType(true, Math.max(root.val, right.maxValue), Math.min(root.val, left.minValue));
+    }
+
+}
+
 
